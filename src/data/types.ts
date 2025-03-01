2575970f6aaa842410ba17ba063c574b17f97604
Martin Pravda <martin.pravda@gmail.com>
2025-03-01 13:36:46 +0100

    data types

---
+++ src/data/types.ts
@@ -0,0 +1,18 @@
+type Rate = {
+  buy: number;
+  middle: number;
+  sell: number;
+  indicator: number;
+  lastModified: string;
+};
+
+type Currency = {
+  currency: string;
+  precision: number;
+  nameI18N?: string;
+  exchangeRate?: Rate;
+  bankNoteRate?: Rate;
+  flags?: string[];
+};
+
+export type {Rate, Currency};
