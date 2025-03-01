fbbd4ebaacec4583f8d33df422877ff9ae83b970
Martin Pravda <martin.pravda@gmail.com>
2025-03-01 11:31:01 +0100

    Initial commit

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import type {Config} from "prettier";
+
+const config: Config = {
+  bracketSpacing: false,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "none"
+};
+
+export default config;
