6e5575f3824f99765edcf822254a0f285dd7c003
Martin Pravda <martin.pravda@gmail.com>
2025-03-03 16:10:37 +0100

    coverage exclude

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import {defineConfig} from "vite";
+import {defineConfig, coverageConfigDefaults} from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    coverage: {
+      exclude: [...coverageConfigDefaults.exclude, "src/**/*.tsx", "src/hooks"]
+    }
+  }
 });
