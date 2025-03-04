b395054745d4905540a34b0209f2171620ec188c
Martin Pravda <martin.pravda@gmail.com>
2025-03-04 17:09:21 +0100

    unknown flag

--- src/CurrencyRow.tsx
+++ src/CurrencyRow.tsx
@@ -1,5 +1,6 @@
-import type {FC} from "react";
+import type {FC, ReactEventHandler} from "react";
 import type {Rate} from "./data/types";
+
 import {NA} from "./constants";
 
 type Props = {
@@ -20,7 +21,12 @@ const CurrencyRow: FC<Props> = ({
 }) => {
   return (
     <li className="row">
-      <img src={`/flags/${countryCode?.toLowerCase()}.png`}></img>
+      <img
+        onError={(event) => {
+          (event.target as HTMLImageElement).src = `/flags/unknown.png`;
+        }}
+        src={`/flags/${countryCode?.toLowerCase()}.png`}
+      ></img>
       <p>{countryName || NA}</p>
       <p>{currency?.toUpperCase() || NA}</p>
       <p>{exchangeRate?.buy ? exchangeRate.buy.toFixed(precision) : NA}</p>
