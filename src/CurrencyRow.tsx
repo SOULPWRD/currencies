ebe582828c4b3942f59d67d80ccc86b15a96831a
Martin Pravda <martin.pravda@gmail.com>
2025-03-04 10:54:52 +0100

    N/A

--- src/CurrencyRow.tsx
+++ src/CurrencyRow.tsx
@@ -1,11 +1,12 @@
 import type {FC} from "react";
 import type {Rate} from "./data/types";
+import {NA} from "./constants";
 
 type Props = {
   exchangeRate?: Rate;
-  countryCode: string;
-  countryName: string;
-  currency: string;
+  countryCode?: string;
+  countryName?: string;
+  currency?: string;
 };
 
 const CurrencyRow: FC<Props> = ({
@@ -16,10 +17,10 @@ const CurrencyRow: FC<Props> = ({
 }) => {
   return (
     <li className="row">
-      <img src={`/flags/${countryCode.toLowerCase()}.png`}></img>
-      <p>{countryName}</p>
-      <p>{currency}</p>
-      <p>{exchangeRate?.buy}</p>
+      <img src={`/flags/${countryCode?.toLowerCase()}.png`}></img>
+      <p>{countryName || NA}</p>
+      <p>{currency || NA}</p>
+      <p>{exchangeRate?.buy || NA}</p>
     </li>
   );
 };
