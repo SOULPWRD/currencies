8aeaced5170824a99f15230a5f08bd855efe3b34
Martin Pravda <martin.pravda@gmail.com>
2025-03-04 11:21:08 +0100

    initial value

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,20 +1,29 @@
 import type {ChangeEventHandler, FC} from "react";
+import {useState} from "react";
 
 type Props = {
+  value?: string;
   label: string;
   onInput: (searchTerm: string) => void;
 };
 
-const Search: FC<Props> = ({onInput, label}) => {
+const Search: FC<Props> = ({onInput, label, value}) => {
+  const [inputValue, setValue] = useState(value);
   const handleInput: ChangeEventHandler<HTMLInputElement> = (event) => {
     const searchTerm = event.target.value;
+    setValue(searchTerm);
     onInput(searchTerm);
   };
 
   return (
     <div className="search">
       <label htmlFor="search">{label}</label>
-      <input id="search" onInput={handleInput} type="text"></input>
+      <input
+        value={inputValue || ""}
+        id="search"
+        onInput={handleInput}
+        type="text"
+      ></input>
     </div>
   );
 };
